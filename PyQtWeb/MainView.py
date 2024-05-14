d7b38f6e63405a6fdcfbeed3dc85cf885fd4f302
Greg Wilding <gregory.wilding@ucalgary.ca>
2024-05-14 11:30:27 -0600

    Adding a button to force page to scroll
    - This probably won't work on a web app where scroll is managed by a script, may need to use a test harness to do that

--- PyQtWeb/MainView.py
+++ PyQtWeb/MainView.py
@@ -1,11 +1,6 @@
-from PyQt6.QtCore import QUrl, pyqtSlot
+from PyQt6.QtCore import QUrl, pyqtSlot, QPointF
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import (
-    QLineEdit,
-    QMainWindow,
-    QPushButton,
-    QToolBar,
-)
+from PyQt6.QtWidgets import (QLineEdit, QMainWindow, QPushButton, QToolBar)
 from PyQt6.QtWebEngineCore import QWebEnginePage
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 
@@ -14,24 +9,29 @@ class MainView(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        initialUrl = "https://possneuro.com"
+        initialUrl = "https://weather.gc.ca/forecast/hourly/ab-52_metric_e.html"
 
         self.backButton = QPushButton()
         self.backButton.setIcon(QIcon(":/qt-project.org/styles/commonstyle/images/left-32.png"))
-        self.backButton.clicked.connect(self.back)
+        self.backButton.clicked.connect(self.backPressed)
 
         self.forwardButton = QPushButton()
         self.forwardButton.setIcon(QIcon(":/qt-project.org/styles/commonstyle/images/right-32.png"))
-        self.forwardButton.clicked.connect(self.forward)
+        self.forwardButton.clicked.connect(self.forwardPressed)
 
         self.addressLineEdit = QLineEdit()
-        self.addressLineEdit.returnPressed.connect(self.load)
+        self.addressLineEdit.returnPressed.connect(self.addressChanged)
         self.addressLineEdit.setText(initialUrl)
+
+        self.scrollButton = QPushButton()
+        self.scrollButton.setText("Scroll Test")
+        self.scrollButton.clicked.connect(self.scrollPressed)
 
         self.toolBar = QToolBar()
         self.toolBar.addWidget(self.backButton)
         self.toolBar.addWidget(self.forwardButton)
         self.toolBar.addWidget(self.addressLineEdit)
+        self.toolBar.addWidget(self.scrollButton)
 
         self.webEngineView = QWebEngineView()
         self.webEngineView.load(QUrl(initialUrl))
@@ -43,19 +43,23 @@ class MainView(QMainWindow):
         self.setWindowTitle("PyQtWebEngine example application")
 
     @pyqtSlot()
-    def load(self):
+    def addressChanged(self):
         url = QUrl.fromUserInput(self.addressLineEdit.text())
         if url.isValid():
             self.webEngineView.load(url)
 
     @pyqtSlot()
-    def back(self):
+    def backPressed(self):
         self.webEngineView.page().triggerAction(QWebEnginePage.WebAction.Back)
 
     @pyqtSlot()
-    def forward(self):
+    def forwardPressed(self):
         self.webEngineView.page().triggerAction(QWebEnginePage.WebAction.Forward)
 
     @pyqtSlot(QUrl)
     def urlChanged(self, url):
         self.addressLineEdit.setText(url.toString())
+
+    @pyqtSlot()
+    def scrollPressed(self):
+        self.webEngineView.page().runJavaScript("window.scrollTo(0, 1000);")
